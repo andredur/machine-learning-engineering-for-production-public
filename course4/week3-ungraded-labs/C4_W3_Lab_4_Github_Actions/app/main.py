5567862e64f64ffe4bfbb6579b28f7aea919f7ba
Andreas Durt <andreas.durt@uantwerpen.be>
2023-08-28 18:07:02 +0200

    reverted python version in workflow

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -5,7 +5,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
 
-#CI/CD Testing
+# CI/CD Testing
+
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
